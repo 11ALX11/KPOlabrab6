b47d0ad822c4ab2ed4972e37cdd6d4090c045bcb
Aleksey <11_alx_11@ro.ru>
2022-05-05 11:03:05 +0300

    task 3

--- z3/main.cpp
+++ z3/main.cpp
@@ -4,6 +4,38 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int lengthOfArray;
+    cin >> lengthOfArray;
+
+    int* inputArray = new int[lengthOfArray];
+    for (int i=0; i<lengthOfArray; i++) {
+        cin >> inputArray[i];
+    }
+
+    int sum = 0;
+    int idxBegin = 0;
+    int bestSum = 0;
+    int bestIdxBegin = 0;
+    int bestIdxEnd = 0;
+    for (int i=0; i<lengthOfArray; i++) {
+        if (sum < 0) {
+            sum = 0;
+            idxBegin = i;
+        }
+
+        sum += inputArray[i];
+
+        if (sum > bestSum) {
+            bestSum = sum;
+            bestIdxBegin = idxBegin;
+            bestIdxEnd = i;
+        }
+    }
+
+    for (int i=bestIdxBegin; i<=bestIdxEnd; i++) {
+        cout << inputArray[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
