df0b4f6e8c13f409ae1d9258f511e369dba19baf
Aleksey <11_alx_11@ro.ru>
2022-05-05 10:22:08 +0300

    task2

--- z2/main.cpp
+++ z2/main.cpp
@@ -4,6 +4,36 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int lengthOfArray;
+    cin >> lengthOfArray;
+
+    int* randomArray = new int[lengthOfArray];
+
+    for (int i=0; i<lengthOfArray; i++) {
+        randomArray[i] = i;
+    }
+
+
+    bool* marked = new bool[lengthOfArray];
+    marked[0]=true;
+    marked[1]=true;
+    for (int i=2; i<lengthOfArray; i++) {
+        marked[i] = false;
+    }
+
+    for (int i=2; i<lengthOfArray; i++) {
+        if (!marked[i]) {
+            int k;
+            for (int j=2; (k=i*j)<lengthOfArray; j++) {
+                marked[k]=true;
+            }
+        }
+    }
+
+    for (int i=0; i<lengthOfArray; i++) {
+        if (!marked[i]) cout << i << " ";
+    }
+    cout << endl;
+
     return 0;
 }
